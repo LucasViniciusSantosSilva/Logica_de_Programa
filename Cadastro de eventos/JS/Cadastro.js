b991b348d24c9ff884bd08b33cbabbcc22e01532
Lucas Vinicius Santos Silva <shakilla.cor@gmail.com>
2022-05-03 10:48:22 -0300

    Ex_cadastro_OK

--- Cadastro de eventos/JS/Cadastro.js
+++ Cadastro de eventos/JS/Cadastro.js
@@ -1,46 +1,54 @@
+var participantes = [];
+var cont1 = 0;
+  
+while (cont1<=100) {   
+    /*Nome*/ 
+    var nome = (window.prompt("Informe seu nome:"));
 
-var participantes = ['Tânia','Julio','Aurelio',];
-var dataAtual = new Date('2022-05-01');
-var nome = (window.prompt("Informe seu nome:"));
-var dia = parseInt(window.prompt("Informe a data do evento(Ano-Mês-Dia):"));
-var cont = 0;
-alert(dataAtual)
-if(dia > dataAtual){
-    alert("Etapa 'OK' - Data -  "+ dia +"");
-    cont++
-}else{
-    alert("- Cadastro não realizado \n - Data não atende aos requesitos  \n - "+ dia +" \n ");  
-} 
-
-var idade = parseFloat(window.prompt("Qual a sua idade"));
-
-if(idade >= 18){
-    alert("Etapa 'OK' - Sua idade é "+ idade +"");
-    cont++
-}else{
-    alert("- Cadastro não realizado \n - Membro menor de idade \n - Menor de idade, sua idade é "+ idade +" \n ");  
-} 
-
-if(cont == 2){   
-    var adicionar = participantes.push("'"+nome+"',")
-
-    alert(""+ participantes +"")
-            
-    alert(""+participantes.length+"/100")     
-
-    var dia = dataAtual.getDate();
-    var mes = (dataAtual.getMonth() + 1);
-    var ano = dataAtual.getFullYear();
-    var horas = dataAtual.getHours();
-    var minutos = dataAtual.getMinutes();
-
-    if(parseInt(participantes.length) <= 100){
-        alert("Evento cadastrada com sucesso!!! \n No dia " + dia + "/" + mes + "/" + ano + ". Agora são " + horas + ":" + minutos + "h. \n Data do evento: "+dia+"\n");
+    /*Data do evento*/
+    var dataAtual = new Date();
+    var dia2 = Date.parse(dataAtual) 
+    var dia = new String(window.prompt("Informe a data do evento(Ano-Mês-Dia):"));
+    var dia1 = Date.parse(dia)
+    var cont = 0;
+    if(dia1 > dia2){
+        alert("Etapa 'OK' - Data -  "+ dia +"");
+        cont++
     }else{
-        alert("- Cadastro não realizado \n - Não permitido por ter excedido o limite de cadastro ");  
+        alert("- Cadastro não realizado \n - Data não atende aos requesitos  \n - "+ dia +" \n ");  
+    } 
+
+    /*Idade do membro*/ 
+    if(cont == 1){
+        var idade = parseFloat(window.prompt("Qual a sua idade"));    
+        if(idade >= 18){
+            alert("Etapa 'OK' - Sua idade é "+ idade +"");
+            cont++
+        }else{
+            alert("- Cadastro não realizado \n - Membro menor de idade \n - Menor de idade, sua idade é "+ idade +" \n ");  
+        } 
     }
-}else{
-    alert("- Cadastro não realizado \n - Data não atende aos requesitos ou membro menor de idade ");  
+
+    /* Atrende todos os requesitos*/
+    if(cont == 2){        
+        var adicionar = participantes.push("'"+nome+"'")   
+        alert(""+ participantes +"")
+                
+        alert(""+participantes.length+"/100 - Participantes")     
+
+        var di = dataAtual.getDate();
+        var mes = (dataAtual.getMonth() + 1);
+        var ano = dataAtual.getFullYear();
+        var horas = dataAtual.getHours();
+        var minutos = dataAtual.getMinutes();
+
+        if(parseInt(participantes.length) <= 100){
+            alert("Evento cadastrada com sucesso!!! \n No dia " + di + "/" + mes + "/" + ano + ". as " + horas + ":" + minutos + "h. \n Data do evento: "+ dia +"");
+            cont1++
+        }else{
+            alert("- Cadastro não realizado \n - Não permitido por ter excedido o limite de cadastro ");  
+        }
+    } else {
+        alert("- Cadastro não realizado \n - Data não atende aos requesitos ou membro menor de idade \n- Tente novamente");  
+    }
 }
-
-
